7cb9e51fff356db2e3950e74603691d0f62db249
Andréss <andrescalix00@gmail.com>
2026-06-07 13:05:57 -0500

    Created sticker model

---
+++ app/src/main/java/com/example/albumtracker/data/model/Player.kt
@@ -0,0 +1,9 @@
+package com.example.albumtracker.data.model
+
+data class Player(
+    val strPlayer: String?,
+    val strNationality: String?,
+    val strTeam: String?,
+    val strPosition: String?,
+    val strThumb: String?
+)
---
+++ app/src/main/java/com/example/albumtracker/data/model/Trade.kt
@@ -0,0 +1,7 @@
+package com.example.albumtracker.data.model
+
+data class Trade(
+    val stickerGiven: Int,
+    val stickerReceived: Int,
+    val tradeDate: String
+)
